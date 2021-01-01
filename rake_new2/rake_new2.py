5633360c06a3a2038afe4ae3fe4b9697797f6e37
Restyled.io <commits@restyled.io>
2021-01-01 19:56:49 +0000

    Restyled by black

--- rake_new2/rake_new2.py
+++ rake_new2/rake_new2.py
@@ -4,23 +4,30 @@ import re
 from collections import Counter, defaultdict
 from itertools import groupby, product, chain
 import nltk
-'''nltk.download('stopwords')
-nltk.download('punkt')'''
+
+"""nltk.download('stopwords')
+nltk.download('punkt')"""
 from nltk.tokenize import wordpunct_tokenize
 
-'''WHY USE wordpunct_tokenize INSTEAD OF word_tokenize ?
+"""WHY USE wordpunct_tokenize INSTEAD OF word_tokenize ?
 -> Example :
 Say, text = "You're silly"
 >> word_tokenize(text) --> ["You","'re","silly"]
 >> wordpunct_tokenize(text) --> ["You","'","re","silly"]
  
-Hence, wordpunct_tokenize > word_tokenize'''
+Hence, wordpunct_tokenize > word_tokenize"""
 
 # the Rake() class
 
 
-class Rake():
-    def __init__(self, stopwords=None, punctuations=None, language='english', keep_html_tags=False):
+class Rake:
+    def __init__(
+        self,
+        stopwords=None,
+        punctuations=None,
+        language="english",
+        keep_html_tags=False,
+    ):
         # Stopwords
         self.stopwords = stopwords
         if self.stopwords is None:
@@ -36,37 +43,40 @@ class Rake():
         # Clubbing all together into one list of indicators where text will be split
         self.ignore_list = list(set(chain(self.stopwords, self.punctuations)))
         # Initializing the calculation metrics
-        self.freq_dist = None       # Frequency distribution of each word in the text
-        self.word_degree = None     # Degrees of keywords
-        self.ranklist = None        # Ranked keywords + their scores
-        self.ranked_kw = None       # Only ranked keywords
+        self.freq_dist = None  # Frequency distribution of each word in the text
+        self.word_degree = None  # Degrees of keywords
+        self.ranklist = None  # Ranked keywords + their scores
+        self.ranked_kw = None  # Only ranked keywords
 
     # Extract keywords
     def get_keywords_from_raw_text(self, text):
         if self.keep_html_tags:
             sentences = nltk.tokenize.sent_tokenize(text)
-            self.get_keywords_from_sent(sentences)   # _______(1)
+            self.get_keywords_from_sent(sentences)  # _______(1)
         else:
-            cleanr = re.compile('<.*?>')
-            cleantext = re.sub(cleanr, '', text)
+            cleanr = re.compile("<.*?>")
+            cleantext = re.sub(cleanr, "", text)
             sentences = nltk.tokenize.sent_tokenize(cleantext)
-            self.get_keywords_from_sent(sentences)   # _______(1)
+            self.get_keywords_from_sent(sentences)  # _______(1)
+
     # (1)
 
     def get_keywords_from_sent(self, sentences):
-        phrase_list = self.get_keywords(sentences)    # ________(2)
+        phrase_list = self.get_keywords(sentences)  # ________(2)
         # Calculation functions
-        self.build_freq_dist(phrase_list)             # ________(3)
-        self.build_word2word_matrix(phrase_list)      # ________(4)
-        self.get_ranklist(phrase_list)                # ________(5)
+        self.build_freq_dist(phrase_list)  # ________(3)
+        self.build_word2word_matrix(phrase_list)  # ________(4)
+        self.get_ranklist(phrase_list)  # ________(5)
+
     # (2)
 
     def get_keywords(self, sentences):
         words = []
         for sent in sentences:
             words.append([w.lower() for w in wordpunct_tokenize(sent)])
-        phrases = self.get_keywords_from_words(words)     # ________(6)
+        phrases = self.get_keywords_from_words(words)  # ________(6)
         return phrases
+
     # (6) --> USER ACCESSIBLE FUNCTION
 
     def get_keywords_from_words(self, words):
@@ -83,11 +93,13 @@ class Rake():
         filtered_groups = groupby(word_list, lambda x: x not in ignore_final)
         keywords_list = [list(grp[1]) for grp in filtered_groups if grp[0]]
         return keywords_list
+
     # (3)
 
     def build_freq_dist(self, phrase_list):
         # Why chain ? Remember that, phrase_list contains multiple keywords_list(refer function (6)).
         self.freq_dist = Counter(chain.from_iterable(phrase_list))
+
     # (4)
 
     def build_word2word_matrix(self, phrase_list):
@@ -103,6 +115,7 @@ class Rake():
         for key in template_graph:
             # For explanation, visit the link memtioned in the README
             self.degree[key] = sum(template_graph[key].values())
+
     # (5)
 
     def get_ranklist(self, phrase_list):
@@ -110,7 +123,7 @@ class Rake():
         for kw in phrase_list:
             rank = 0.0
             for word in kw:
-                rank += self.degree[word]/self.freq_dist[word]
+                rank += self.degree[word] / self.freq_dist[word]
             self.ranklist.append((rank, " ".join(kw)))
         # Sorting in descending order of ranks and preparing keywords + scores
         self.ranked_kw = [kwd[1] for kwd in self.ranklist]
@@ -120,14 +133,17 @@ class Rake():
 
     def get_ranked_keywords(self):
         return set(self.ranked_kw)
+
     # (B)
 
     def get_keywords_with_scores(self):
         return set(self.ranklist)
+
     # (C)
 
     def get_word_freq(self):
         return self.freq_dist
+
     # (D)
 
     def get_kw_degree(self):
@@ -138,7 +154,7 @@ class Rake():
         print()
 
 
-'''# Test run cases
+"""# Test run cases
 text = "Red apples are good in taste."
 rake = Rake()
 rake.get_keywords_from_raw_text(text)
@@ -173,4 +189,4 @@ print("Keeping the tags : ",kw3)
 rake4.get_keywords_from_raw_text(text2)
 kw_s4 = rake4.get_keywords_with_scores()
 kw4 = rake4.get_ranked_keywords()
-print("Eliminating the tags : ",kw4)'''
+print("Eliminating the tags : ",kw4)"""
